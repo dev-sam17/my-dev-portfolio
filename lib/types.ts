d2432b1348dcb9f01996b4c36d057dc9ec40575d
Saurav Kumar <codewizard.saurav@gmail.com>
2025-06-03 21:19:57 +0530

    updated types

--- lib/types.ts
+++ lib/types.ts
@@ -1,9 +1,9 @@
 import { Prisma } from "./prisma";
 
 export type Project = Prisma.Project;
-export type ProjectForm = Omit<Project, "id">;
+export type ProjectForm = Omit<Project, "id" | "images" | "demoUrl" | "githubUrl">;
 
 
 export type FreelanceProject = Prisma.FreelanceProject;
-export type FreelanceProjectForm = Omit<FreelanceProject, "id">;
+export type FreelanceProjectForm = Omit<FreelanceProject, "id" | "images" | "demoUrl" | "githubUrl">;
 
