e7428ac04ece0a7d96eea08948a4da48bf015a14
Saurav Kumar <codewizard.saurav@gmail.com>
2025-06-04 12:30:34 +0530

    changed bg gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-
 export const metadata: Metadata = {
   title: "Saurav Kumar",
   description: "Created In NextJS",
