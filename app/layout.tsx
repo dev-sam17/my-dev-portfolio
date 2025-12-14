8b426e423f3ac6f439871e2864f6eca6ae39c179
Saurav Kumar <codewizard.saurav@gmail.com>
2025-12-14 13:07:51 +0530

    Add Google Analytics tracking with gtag.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import NextAuthSessionProvider from "@/app/providers/session-provider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Saurav Kumar",
@@ -16,7 +17,22 @@ export default function RootLayout({
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
+        <head>
+          {/* Google tag (gtag.js) */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6QVQ96HBP6"
+          ></Script>
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6QVQ96HBP6');
+  `}
+          </Script>
+        </head>
         <body className="bg-black">
           <ThemeProvider
             attribute="class"
